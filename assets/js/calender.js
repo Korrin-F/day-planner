d8c59507cafff23efb0b0f635289b24a05dcbcfe
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2023-12-27 18:06:37 +0000

    add advancedFormat plugin and update the date

--- assets/js/calender.js
+++ assets/js/calender.js
@@ -1,4 +1,5 @@
+
 // todays date time
 let today = dayjs();
 // update the currentDay id with todays date in format Tue, Jan 01st 2024
-$("#currentDay").text(today.format("ddd, D MMMM YYYY"))
+$("#currentDay").text(today.format("ddd, Do MMMM YYYY"))
